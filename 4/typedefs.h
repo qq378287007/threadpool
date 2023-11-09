8229c300bfc4d45f7c319f9a2d89db4575b13dbe
qq378287007 <qq378287007@163.com>
2023-11-09 17:14:00 +0800

    a

---
+++ 4/typedefs.h
@@ -0,0 +1,20 @@
+#ifndef __TYPE_DEFS__H_
+#define __TYPE_DEFS__H_
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#ifndef BOOL
+#define BOOL unsigned char
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#endif
